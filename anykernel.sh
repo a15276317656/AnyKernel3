70773f21b512794e02cf79a17061d1d00f957fac
a15276317656 <119063905+a15276317656@users.noreply.github.com>
2023-10-03 05:19:31 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,8 +4,8 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
-do.devicecheck=1
+kernel.string=oppo_a72-1.0 @ xda-developers
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
@@ -29,8 +29,8 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
 
 # boot shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-is_slot_device=0;
+block=auto;
+is_slot_device=auto;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
